d485ffdf1f282f6954ae9367b50a81f109c92676
priyankachadar <61162836+priyankachadar@users.noreply.github.com>
2021-02-05 00:47:51 +0530

    single/ multiple

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-var React = require("react");
-var ReactDom = require("react-dom");
+import React from "react";
+import ReactDom from "react-dom";
 
+//ReactDom.render(<h1>HELLO WORLD</h1>, document.getElementById("root"));
+
+ReactDom.render(
+  <div>
+    <h1>HELLO WORLD</h1>,<p> this is paragraph </p>
+  </div>,
+  document.getElementById("root")
+);
